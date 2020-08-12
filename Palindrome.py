0e8e6c6e0476e56c38e48d1f79db039e4dc977f0
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2020-08-12 09:36:35 +0530

    Update Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -8,7 +8,6 @@ Created on Tue Aug 11 20:05:12 2020
 
 ''' Method 1 using an extra variable '''
 
-'''
 word = "malayalam"
 x = ""
 for i in range(0, len(word)):
@@ -19,23 +18,32 @@ if (x == word):
     print("Yes its a Palindrome")
 else:
     print("No, its NOT a Palindrome")
-'''
+
     
 ''' Method 2 using slicing '''
 
-'''
 word = "malayalaam"
 reversed_word = word[:: -1]
 if word == reversed_word:
     print("Yes, its a palindrome")
 else:
     print("No, its NOT a palindrome")
-'''
+
 
 ''' Method 3 using RECURSION '''
-def reverse_string(string):
+def reverse(string):
     # base case
     if len(string) == 0:
         return string
     else:
-        return reverse_string()
+        return reverse(string[1:]) + string[0]
+    
+def palindrome(a):
+    if a == reversed_string:
+        print("Yes, it is a Palindrome")
+    else:
+        print("No, its NOT a palindrome")
+
+a = "malayalam"
+reversed_string = reverse(a)
+palindrome(a)
