f117b5acf5446842c462871dd0c14b30083ff04b
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2020-08-14 00:46:11 +0530

    Add files via upload

---
+++ Josephus Problem.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Josephus Problem using Circular Linked List
+
+Created on Fri Aug 14 00:40:26 2020
+
+@author: Abin
+"""
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
