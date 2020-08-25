b0e0b40efccd6d1cd1f3f19b550f4db8f17f79dd
Abin (אבין ברגיס) <36173893+Spartan-119@users.noreply.github.com>
2020-08-25 14:35:31 +0530

    Update Intro to Dictionaries.py

--- Intro to Dictionaries.py
+++ Intro to Dictionaries.py
@@ -5,12 +5,46 @@ Intro to Dictionaries
 
 Created on Mon Aug 24 18:32:05 2020
 
-@author: abin
+@author: Abin
 """
+'''    
+d = {"one":"ek", "two":"do", "three":"teen"}
 
+print()
+
+eng2spn = dict()
+print("This will print an empty dictionary: ", eng2spn)
+print(len(d))
+print("one" in d)
+print()
+
+vals = d.values()
+
+print(vals)
+print()
+'''
+def histogram(s):
+    d = dict()
+    for c in s:
+        if c not in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+    return d
+
+def print_hist(h):
+    for key in sorted(h):
+        print(key, h[key])
+
+verse = "The Lord is my shepherd: I shall not want."
+h = histogram(verse)
+print_hist(h)
+
+# print(histogram("abinvarghese"))
+
+# REVERSE LOOKUP
 def reverse_lookup(d, v):
     for k in d:
         if d[k] == v:
             return k
-    raise LookupError
-    
+    raise LookupError("Value does NOT appear in the dictionary")
